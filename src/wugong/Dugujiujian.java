e48b3f60a85f7b32a619db5620ac266adf125813
songxuewang <292404585@qq.com>
2019-12-05 19:55:28 +0800

    删除了几行

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -4,9 +4,8 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("111111111");
-		System.out.println("11113333311111");
-		System.out.println("2222222");
-		System.out.println("111114444444441111");
+		
+		
 	}
 
 }
